e3e088662b7cb7326962984b5addf50ddfa5f8ba
aifeigin <aifeigin@yahoo.com>
2015-06-25 11:50:28 +0300

    Simplify console output

--- Stage1/Program.cs
+++ Stage1/Program.cs
@@ -17,16 +17,14 @@ namespace Stage1
                 var demo = new Demo.Solution();
                 int[] A = new int[] { Int32.MaxValue, Int32.MaxValue, Int32.MaxValue, Int32.MaxValue, Int32.MaxValue, Int32.MaxValue, Int32.MaxValue, Int32.MaxValue, Int32.MaxValue };
                 Console.WriteLine(demo.solution(A));
-                Console.WriteLine("Demo passed. Press any key to finish.");
-                Console.ReadLine();
+                Console.WriteLine("Demo passed");
             }
             if(args[0]=="test1")
             { 
                 var test1 = new Test1.Solution();
                 Console.WriteLine(test1.Test1());
                 Console.WriteLine(test1.Test2());
-                Console.WriteLine("Test 1 passed. Press any key to finish.");
-                Console.ReadLine();
+                Console.WriteLine("Test 1 passed");
             }
             if (args[0] == "test2")
             {
@@ -34,8 +32,7 @@ namespace Stage1
                 test2.Test1();
                 test2.Test2();
                 test2.Test3();
-                Console.WriteLine("Test 2 passed. Press any key to finish.");
-                Console.ReadLine();
+                Console.WriteLine("Test 2 passed");
             }
             if (args[0] == "test3")
             {
@@ -45,10 +42,10 @@ namespace Stage1
            catch(Exception ex)
            {
                Console.WriteLine("Something wrong");
-               Console.WriteLine(ex.Message);
-               Console.WriteLine("Press any key to finish");
-               Console.ReadLine();
+               Console.WriteLine(ex.Message);               
            }
+           Console.WriteLine("Press any key to finish");
+           Console.ReadLine();
         }
     }
 }
